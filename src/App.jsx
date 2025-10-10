dd8b5c7cd6b0a666bd473712a9bfe28532a76a15
Augustineku <churchfathers@gmail.com>
2025-10-10 21:26:48 +0900

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,106 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Gallery from "./Gallery.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const images = [
-    "https://picsum.photos/200?random=1",
-    "https://picsum.photos/200?random=2",
-    "https://picsum.photos/200?random=3",
-  ];
+  const [linkShops, setLinkshops] = useState([]);
+
+  useEffect(() => {
+    fetch("https://linkshop-api.vercel.app/19-10/linkshops").then((res) => {
+      setLinkshops(res.list);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>LINK SHOP</h1>
-      <input type="text" value="샵 이름으로 겁색해 보세요"></input>
-      <Gallery images={images} />
-    </div>
+    <>
+      <div className="container">
+        <header>
+          <h1>LINK SHOP</h1>
+          <a>생성하기</a>
+        </header>
+
+        <input type="text" value="샵 이름으로 겁색해 보세요"></input>
+        {linkShops.map((item) => (
+          <Gallery
+            key={item.id}
+            name={item.name}
+            urlName={item.shop.urlName}
+            likes={item.likes}
+            productsCount={item.products}
+            products={item.products}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
 export default App;
+
+var mockdata = [
+  {
+    id: 920,
+    name: "너구리 직구 상점",
+    userId: "dddd111",
+    shop: {
+      id: 940,
+      shopUrl: "https://example.com/...",
+      urlName: "string",
+      imageUrl: "https://example.com/...",
+    },
+    likes: 0,
+    teamId: "19-10",
+    productsCount: 1,
+    products: [
+      {
+        id: 3504,
+        imageUrl: "https://example.com/...",
+        name: "너구리 직구상점",
+      },
+    ],
+  },
+  {
+    id: 919,
+    name: "너구리 직구 상점",
+    userId: "dddd",
+    shop: {
+      id: 939,
+      shopUrl: "https://example.com/...",
+      urlName: "string",
+      imageUrl: "https://example.com/...",
+    },
+    likes: 0,
+    teamId: "19-10",
+    productsCount: 1,
+    products: [
+      {
+        id: 3503,
+        imageUrl: "https://example.com/...",
+        name: "너구리 직구상점",
+      },
+    ],
+  },
+  {
+    id: 918,
+    name: "dfdf111",
+    userId: "fdjf83hfid",
+    shop: {
+      id: 938,
+      shopUrl: "https://example.com/...",
+      urlName: "string",
+      imageUrl: "https://example.com/...",
+    },
+    likes: 0,
+    teamId: "19-10",
+    productsCount: 1,
+    products: [
+      {
+        id: 3502,
+        imageUrl: "https://example.com/...",
+        name: "string",
+      },
+    ],
+  },
+];
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,13 +1,94 @@
 import styles from "./Gallery.module.css";
 
-function Gallery({ images }) {
+function Gallery({ name, urlName, likes, productsCount, products }) {
   return (
     <div className={styles.gallery}>
-      {images.map((image) => (
-        <img key={image} src={image} />
-      ))}
+      <div>
+        <div>
+          <img />
+          <div>
+            <div>{name}</div>
+            <div>{urlName}</div>
+          </div>
+        </div>
+        <div>{likes}</div>
+      </div>
+      <div>대표 상품 {productsCount}</div>
+      <div>
+        {products.map((product) => (
+          <img src={product.imageUrl} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Gallery;
+// {
+//   "list": [
+//     {
+//       "id": 920,
+//       "name": "너구리 직구 상점",
+//       "userId": "dddd111",
+//       "shop": {
+//         "id": 940,
+//         "shopUrl": "https://example.com/...",
+//         "urlName": "string",
+//         "imageUrl": "https://example.com/..."
+//       },
+//       "likes": 0,
+//       "teamId": "19-10",
+//       "productsCount": 1,
+//       "products": [
+//         {
+//           "id": 3504,
+//           "imageUrl": "https://example.com/...",
+//           "name": "너구리 직구상점"
+//         }
+//       ]
+//     },
+//     {
+//       "id": 919,
+//       "name": "너구리 직구 상점",
+//       "userId": "dddd",
+//       "shop": {
+//         "id": 939,
+//         "shopUrl": "https://example.com/...",
+//         "urlName": "string",
+//         "imageUrl": "https://example.com/..."
+//       },
+//       "likes": 0,
+//       "teamId": "19-10",
+//       "productsCount": 1,
+//       "products": [
+//         {
+//           "id": 3503,
+//           "imageUrl": "https://example.com/...",
+//           "name": "너구리 직구상점"
+//         }
+//       ]
+//     },
+//     {
+//       "id": 918,
+//       "name": "dfdf111",
+//       "userId": "fdjf83hfid",
+//       "shop": {
+//         "id": 938,
+//         "shopUrl": "https://example.com/...",
+//         "urlName": "string",
+//         "imageUrl": "https://example.com/..."
+//       },
+//       "likes": 0,
+//       "teamId": "19-10",
+//       "productsCount": 1,
+//       "products": [
+//         {
+//           "id": 3502,
+//           "imageUrl": "https://example.com/...",
+//           "name": "string"
+//         }
+//       ]
+//     }
+//   ],
+//   "nextCursor": null
+// }
